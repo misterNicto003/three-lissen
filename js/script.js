2705cbf65c6a94f745d6130d06dcf96ce099adb8
Mlovz <example@gmail.com>
2022-07-08 01:29:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+if (4 == 3) {
+    console.log('ok');
+} else {
+    console.log('not');
+}
+
+
+
+/*if (num < 49) {
+    console.log('not');
+} else if (num > 100) {
+    console.log(' manu');
+} else {
+    console.log('ok');
+}
+
+
+(num === 50) ? console.log('ok') : console.log('error');/**/
+
+
+
+const num = 54;
+
+switch (num) {
+    case 10:
+        console.log('неверно');
+        break;
+    case 30:
+        console.log('неверно');
+        break;
+    case 50:
+        console.log('верно');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;
+
+}
+
+
+
+
+
+
+9
